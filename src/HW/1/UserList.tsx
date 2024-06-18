f23967779e5a27ef1118abbfb7e90d3aa840b269
Aliaksei Holad <aleksei-golod@mail.ru>
2024-06-18 22:53:11 +0300

    Refactoring HW1

--- src/HW/1/UserList.tsx
+++ src/HW/1/UserList.tsx
@@ -1,32 +1,35 @@
 type AddressType = {
-  street: string;
-  city: string;
+    street: string;
+    city: string;
 };
 
 type UserType = {
-  id: number
-  name: string
-  age: number
-  address: AddressType
+    id: number
+    name: string
+    age: number
+    address: AddressType
 };
 
 type UserListPropsType = {
-  users: UserType[];
+    users: UserType[];
 };
 
 export const UserList = ({users}: UserListPropsType) => {
-  return (
-    <div id={'hw01-users'}>
-      <h2>User List:</h2>
+    const mappedUsers = users.map((user) =>
+        (
+            <li key={user.id} id={`hw01-user-${user.id}`}>
+                <strong>{user.name}</strong> (Age: {user.age})<strong> Address:</strong>
+                {user.address.street}, {user.address.city}
+            </li>
+        ));
 
-      <ul>
-        {users.map((user) => (
-          <li key={user.id} id={`hw01-user-${user.id}`}>
-            <strong>{user.name}</strong> (Age: {user.age})<strong> Address:</strong>
-            {user.address.street}, {user.address.city}
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+    return (
+        <div id={'hw01-users'}>
+            <h2>User List:</h2>
+
+            <ul>
+                {mappedUsers}
+            </ul>
+        </div>
+    );
 };
